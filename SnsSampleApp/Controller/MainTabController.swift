5b142a97e079dce311a782a443e52da79621aa8c
tk0929 <bluecoffin929@gmail.com>
2021-01-05 23:29:50 +0900

    [add] メインウィンドウの起動設定を追記

---
+++ SnsSampleApp/Controller/MainTabController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabController.swift
+//  SnsSampleApp
+//
+//  Created by t.koike on 2021/01/02.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}

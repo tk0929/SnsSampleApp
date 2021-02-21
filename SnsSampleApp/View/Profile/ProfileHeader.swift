35dc44f7f60fafc2f4b2fca78e3c0af38075b43b
tk0929 <bluecoffin929@gmail.com>
2021-02-21 23:19:23 +0900

    [add] ProfileHederにprofileImageView,nameLabel,editProfileButtonを追加

--- SnsSampleApp/View/Profile/ProfileHeader.swift
+++ SnsSampleApp/View/Profile/ProfileHeader.swift
@@ -10,13 +10,59 @@ import UIKit
 class ProfileHeader: UICollectionReusableView {
     
     //MARK: - Propaerties
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.image = #imageLiteral(resourceName: "ProfileImage")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.setDimensions(height: 80, width: 80)
+        imageView.layer.cornerRadius = 80 / 2
+        
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "ここに名前が入る"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        
+        return label
+    }()
+    
+    private lazy var editProfileButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("プロフィールを編集", for: .normal)
+        button.layer.cornerRadius = 3
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.borderWidth = 0.5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(teppedEditProfileButton), for: .touchUpInside)
+        
+       return button
+    }()
+    
+    
     
     //MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .systemRed
+        backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 16, paddingLeft: 12)
+        
+        
+        addSubview(nameLabel)
+        nameLabel.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 12)
+        
+        addSubview(editProfileButton)
+        editProfileButton.anchor(top: nameLabel.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 16, paddingLeft: 24, paddingRight: 24)
         
         
     }
@@ -25,6 +71,12 @@ class ProfileHeader: UICollectionReusableView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    //MARK: - Actions
     
+    @objc private func teppedEditProfileButton() {
+        
+        print("teppedEditProfileButton")
+        
+    }
     
 }

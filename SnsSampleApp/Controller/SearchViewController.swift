997f7c77315ae1e40fc02e6eca457cdbdfe4414f
tk0929 <bluecoffin929@gmail.com>
2021-02-25 22:43:19 +0900

    [add] #13 searchController上部にユーザ検索バーの実装

--- SnsSampleApp/Controller/SearchViewController.swift
+++ SnsSampleApp/Controller/SearchViewController.swift
@@ -11,9 +11,14 @@ class SearchViewController: UITableViewController {
     
     //MARK: - Propaerties
     private let reuseIdentifier = "UserCell"
-    
     private var users = [User]()
+    private var filteredUsers = [User]()
+    //   検索バー
+    private let searchController = UISearchController(searchResultsController: nil)
     
+    private var isSearchMode: Bool {
+        return searchController.isActive && !searchController.searchBar.text!.isEmpty
+    }
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
@@ -21,53 +26,70 @@ class SearchViewController: UITableViewController {
         
         confitureTableView()
         fetchUsers()
+        configureSearchController()
         
     }
     
     //MARK: - Helpers
     
     func confitureTableView() {
-        
         tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 64
+    }
+    
+    func configureSearchController() {
+        
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "検索"
+        navigationItem.searchController = searchController
+        definesPresentationContext = false
         
     }
     
+    
     //MARK: - API
+    
     func fetchUsers() {
         
         UserService.fetchUsersData { (users) in
             self.users = users
             self.tableView.reloadData()
+            
         }
     }
     
 }
 
-    //MARK: - UITableViewDataSource
+//MARK: - UITableViewDataSource
 
 extension SearchViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        return isSearchMode ? filteredUsers.count : users.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier , for: indexPath) as! UserCell
-        cell.viewModel = UserCellViewModel(user: users[indexPath.row])
+        
+        let user = isSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
+        cell.viewModel = UserCellViewModel(user: user)
+        
         return cell
         
     }
     
 }
 
-    //MARK: - UITableViewDataSource
+//MARK: - UITableViewDataSource
 
 extension SearchViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        let controller = ProfileViewController(user: users[indexPath.row])
+        let user = isSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
+        let controller = ProfileViewController(user: user)
         navigationController?.pushViewController(controller, animated: true)
         
     }
@@ -75,6 +97,22 @@ extension SearchViewController {
     
 }
 
+//MARK: - UISearchResultsUpdating
 
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        guard let searchText = searchController.searchBar.text?.lowercased() else { return }
+        
+        filteredUsers = users.filter({
+            $0.userName.contains(searchText) ||
+                $0.fullName.lowercased().contains(searchText)
+        })
+
+        self.tableView.reloadData()
+    }
+    
+    
+}
 
 

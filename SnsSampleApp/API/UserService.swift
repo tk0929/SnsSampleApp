1ca579060bf73ae4ce7cfca3a0b09c1e2e20fddc
tk0929 <73379493+tk0929@users.noreply.github.com>
2021-02-25 22:48:07 +0900

    #13 (#18)
    * [update]  #13  Firestoreからすべてのユーザー情報を取得するfetchUsersDataを追加
    
    * [add] #13 SearchViewControllerのUI作成
    
    * [update] #13 セルのタップ時に対応したユーザのプロフィールを表示する
    
    * [add] #13 searchController上部にユーザ検索バーの実装
    
    * #13 完了

--- SnsSampleApp/API/UserService.swift
+++ SnsSampleApp/API/UserService.swift
@@ -8,7 +8,7 @@
 import Firebase
 
 struct UserService {
-    
+// エラー時の処理も追記するべき
     static func fetchCurrentUserData(complition: @escaping(User) -> Void) {
         
         guard let uid = Auth.auth().currentUser?.uid else { return }
@@ -21,5 +21,18 @@ struct UserService {
         }
     }
     
+    
+    static func fetchUsersData(completion: @escaping([User]) -> Void) {
+        COLLECTION_USERS.getDocuments { (snapShot, error) in
+          
+            guard let snapShot = snapShot else { return }
+            
+            let users = snapShot.documents.map{( User(dictionary: $0.data() ))}
+            completion(users)
+            
+        }
+        
+        
+    }
     
 }
--- SnsSampleApp/Controller/SearchViewController.swift
+++ SnsSampleApp/Controller/SearchViewController.swift
@@ -7,15 +7,112 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
-
+class SearchViewController: UITableViewController {
+    
+    //MARK: - Propaerties
+    private let reuseIdentifier = "UserCell"
+    private var users = [User]()
+    private var filteredUsers = [User]()
+    //   検索バー
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    private var isSearchMode: Bool {
+        return searchController.isActive && !searchController.searchBar.text!.isEmpty
+    }
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        confitureTableView()
+        fetchUsers()
+        configureSearchController()
         
+    }
+    
+    //MARK: - Helpers
+    
+    func confitureTableView() {
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 64
+    }
+    
+    func configureSearchController() {
+        
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "検索"
+        navigationItem.searchController = searchController
+        definesPresentationContext = false
+        
+    }
+    
+    
+    //MARK: - API
+    
+    func fetchUsers() {
+        
+        UserService.fetchUsersData { (users) in
+            self.users = users
+            self.tableView.reloadData()
+            
+        }
+    }
+    
+}
+
+//MARK: - UITableViewDataSource
+
+extension SearchViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isSearchMode ? filteredUsers.count : users.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier , for: indexPath) as! UserCell
+        
+        let user = isSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
+        cell.viewModel = UserCellViewModel(user: user)
+        
+        return cell
+        
+    }
+    
+}
+
+//MARK: - UITableViewDataSource
+
+extension SearchViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let user = isSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
+        let controller = ProfileViewController(user: user)
+        navigationController?.pushViewController(controller, animated: true)
         
     }
     
     
 }
+
+//MARK: - UISearchResultsUpdating
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        guard let searchText = searchController.searchBar.text?.lowercased() else { return }
+        
+        filteredUsers = users.filter({
+            $0.userName.contains(searchText) ||
+                $0.fullName.lowercased().contains(searchText)
+        })
+
+        self.tableView.reloadData()
+    }
+    
+    
+}
+
+
